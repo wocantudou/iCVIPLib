8851c16e4226c1fa0255a219ea834276c2c76c41
wensha2 <shawen@mail.ustc.edu.cn>
2024-06-15 11:21:39 +0800

    [add] run test case sucessful

--- source/iCVIPLibApi/iCVIPLibApi.h
+++ source/iCVIPLibApi/iCVIPLibApi.h
@@ -2,6 +2,14 @@
 #include <opencv2/opencv.hpp>
 // Keep the input parameters and return values of the encapsulated function
 // consistent with the native OpenCV function
+
+#define iCVimread ICV_SAFE_OPENCV::safe_imread
+#define iCVimwrite ICV_SAFE_OPENCV::safe_imwrite
+#define iCVresize ICV_SAFE_OPENCV::safe_resize
+#define iCVcvtColor ICV_SAFE_OPENCV::safe_cvtColor
+#define iCVcrop ICV_SAFE_OPENCV::safe_crop
+#define iCVsub_mean_and_divide_std ICV_SAFE_OPENCV::sub_mean_and_divide_std
+
 namespace ICV_SAFE_OPENCV {
 enum CHANNEL_SEPARATION_TYPE {
     CHANNEL_SEPARATION_TYPE_NONE,
--- source/iCVIPLibTest/iCVIPLibTest.cpp
+++ source/iCVIPLibTest/iCVIPLibTest.cpp
@@ -1,6 +1,41 @@
 #include "iCVIPLibApi.h"
+#include <iostream>
 #include <stdlib.h>
-int main() {
+
+#define CHECK_IMAGE_EMPTY(img)                                                 \
+    if (img.empty()) {                                                         \
+        std::cerr << "Opencv's Mat is empty." << std::endl;                    \
+        return EXIT_FAILURE;                                                   \
+    }
+#define CHECK_RETURN_BOOL(flag)                                                \
+    if (!flag) {                                                               \
+        std::cerr << "Bool : The function failed to be executed."              \
+                  << std::endl;                                                \
+        return EXIT_FAILURE;                                                   \
+    }
+#define CHECK_RETURN_INT(ret)                                                  \
+    if (ret) {                                                                 \
+        std::cerr << "Integer : The function failed to be executed."           \
+                  << std::endl;                                                \
+        return EXIT_FAILURE;                                                   \
+    }
+int main(int argc, char *argv[]) {
     int ret = EXIT_SUCCESS;
+    // 检查参数个数是否正确
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file> <output_file>"
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
+    // 读取文件路径和保存文件路径
+    std::string input_file = argv[1];
+    std::string output_file = argv[2];
     std::cout << "Hello World" << std::endl;
+    cv::Mat img = iCVimread(input_file);
+    CHECK_IMAGE_EMPTY(img);
+    cv::Mat img_rsz = iCVresize(img, cv::Size(100, 100));
+    CHECK_IMAGE_EMPTY(img);
+    bool flag = iCVimwrite(output_file, img_rsz);
+    CHECK_RETURN_BOOL(flag);
+    return ret;
 }
